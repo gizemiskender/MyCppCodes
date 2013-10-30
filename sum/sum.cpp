83e011b717dcb2aed1fab5673f6907e6ca597698
gizem <gizem@gizem-Inspiron-5521.(none)>
2013-10-30 22:46:23 +0200

    sum

---
+++ sum/sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int sum(int x, int y);
+
+int main()
+{
+    cout << sum(1,50) << endl;
+    return 0;
+}
+int sum(int x, int y)
+{
+    if(x > y)
+    {
+        return 0;
+    }
+    else
+    {
+        return x + sum(x+1,y);
+    }
+}

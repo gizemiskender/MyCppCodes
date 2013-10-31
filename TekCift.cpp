95deb931e08d08b34176f13d537b7d98e934e528
gizem <gizem@gizem-Inspiron-5521.(none)>
2013-10-31 15:57:43 +0200

    cpp codes exemples

---
+++ TekCift.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int mod(int x);
+
+int main()
+{
+    cout << mod(122) << endl;
+    return 0;
+}
+int mod(int x)
+{
+    if( x & 1 == 1)
+        cout << "tek" << endl;
+    else
+        cout << "cift" <<endl;
+}
---
+++ recuCarp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int ust(int x, int y);
+
+int main()
+{
+    cout << ust(2,5) << endl;
+    return 0;
+}
+int ust(int x, int y){
+
+    if(y == 0)
+    {
+      return 1;
+    }
+    else
+    {
+       return x * ust(x, y-1);
+    }
+}
+
